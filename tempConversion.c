32898d02bb29f22f7079e0a8b1823bdf744a176a
Recelis <jackymlui@live.com>
2022-04-07 21:31:37 +1000

    rewrote with symbolic constants

--- tempConversion.c
+++ tempConversion.c
@@ -3,19 +3,19 @@
 /*
 "print Fahrenheight-Ceisius table for fahr = 0, 20, ..., 300 " */
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 main ()
 {
     float fahr, celsius;
     float lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
-    while (fahr < upper) {
+    fahr = LOWER;
+    while (fahr < UPPER) {
         celsius = 5.0 / 9.0 * (fahr-32);
         printf("%3.0f\t%6.1f\n", fahr, celsius);
-        fahr = fahr + step;
+        fahr = fahr + STEP;
     }
 }
